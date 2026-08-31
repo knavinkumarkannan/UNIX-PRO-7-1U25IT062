3969da6c215b4135b999973f41d9a10b36651f29
knavinkumarkannan <knavinkumar47@gmail.com>
2026-08-31 19:56:36 +0530

    Unix -PRO 7-1U25IT062

--- student_solution.sh
+++ student_solution.sh
@@ -3,8 +3,11 @@
 # Test files and directories create panron
 touch filename.txt file1.txt file2.txt file3.txt
 mkdir -p directory_name
+mkdir -p my_folder/sub_folder
+touch my_folder/file.txt
 
 # Files & Directories remove panron
 rm filename.txt
 rm file1.txt file2.txt file3.txt
 rmdir directory_name
+rm -r my_folder
